cce80da6e97a6454b1ef6e7edffe286c30f178e8
Constance BENOIT <cbenoi15@ds2.CS.McGill.CA>
2017-11-28 20:36:15 -0500

    final version, please read the description

--- computeWeights.m
+++ computeWeights.m
@@ -1,62 +1,74 @@
 function crsp = computeWeights(crsp)
+
+%% this function appends to crsp
+%It computes the weight that should be invested in a stock at a point in
+%time. It takes into account previous weight in the stock. When selling,
+%sell the full weight of the stock. When buying, buy based on available
+%portfolio weight liberated by selling at that point in time.
+
+%%
     
-    dateList = sort(unique(crsp.datetime));
+    dateList = sort(unique(crsp.datenum));
     
-    crsp = sortrows(crsp, {'PERMNO', 'datetime'});
+    crsp = sortrows(crsp, {'PERMNO', 'datenum'});
     
-    crsp{:, 'stockWeights'} = Nan;
+    crsp{:, 'stockWeights'} = NaN;
     
     %handle first row (still work to do)
-    %crsp.stockWeight(1) = 1/sum(crsp.datenum == crsp.datenum(1) & crsp.buy ==1);
+    %crsp.stockWeight(1) = 1/sum(crsp.datenum == crsp.datenum(1) & crsp.Buy ==1);
     
     for i = 2: height(crsp)
         
         if(crsp.PERMNO(i-1) == crsp.PERMNO(i))
             
-            if(crsp.stockWeight(i-1) == 0 && crsp.buy(i) ==1)
+            if(crsp.stockWeights(i-1) == 0 && crsp.Buy(i) ==1)
                 
                 %total weight of the portfolio sold at this date
-                sales = sum(crsp.stockWeight(crsp.datenum == crsp.datenum(i-1) & crsp.stockWeight>0 & crsp.sell ==1));
+                whichRows = crsp.datenum == crsp.datenum(i)& crsp.stockWeights>0 & crsp.Sell ==1;
+                sales = nansum(crsp.stockWeights(whichRows));
                 
-                %number (integer) of new stocks purchased
-                newStocksPurchased = sum(crsp.buy(crsp.datenum == crsp.datenum(i-1)& crsp.stockWeight==0));
+                if (sales == 0)
+                    currentWeighting  = 1/nansum(crsp.Buy(crsp.datenum == crsp.datenum(i)));
+                else
+                    %number (integer) of new stocks purchased
+                    newStocksPurchased = nansum(crsp.Buy(crsp.datenum == crsp.datenum(i)& crsp.stockWeights==0));
                 
-                %weight invested in new purchases at this date
-                currentWeighting = (1 - sales)/ newStocksPurchased;
-                
+                    %weight invested in new purchases at this date
+                    currentWeighting = (1 - sales)/ newStocksPurchased;
+                end
                 %value assignment
-                crsp.stockWeight(i) = crsp.buy(i)*currentWeighting;
+                crsp.stockWeights(i) = currentWeighting;
             
-            elseif (crsp.sell(i)==1)
-                crsp.stockWeight(i) = 0;
+            elseif (crsp.Sell(i)==1)
+                crsp.stockWeights(i) = 0;
             else
-                crsp.stockWeight(i) = crsp.stockWeight(i-1);
+                crsp.stockWeights(i) = crsp.stockWeights(i-1);
             end
         else
             %case where this is the min time in framework
-            if(crsp.buy(i) ==1 && crsp.datetime(i) ==dateList(1))
-                crsp.stockWeight(i) = 1/sum(crsp.datenum == dateList(1) & crsp.buy ==1);
+            if(crsp.Buy(i) ==1 && crsp.datenum(i) ==dateList(1))
+                crsp.stockWeights(i) = 1/nansum(crsp.Buy(crsp.datenum == dateList(1) & crsp.Buy ==1));
            
-            elseif (crsp.buy(i) ==1)
+            elseif (crsp.Buy(i) ==1)
                 
-                idx = dateList == crsp.datetime(i);
+                idx = dateList == crsp.datenum(i);
                 thisDate = dateList(idx-1);
                     
                 
-                %total weight of the portfolio sold at this date
-                sales = sum(crsp.stockWeight(crsp.datenum == thisDate) & crsp.stockWeight>0 & crsp.sell ==1));
+                %total weight of the portfolio sold on this date
+                sales = nansum(crsp.stockWeights(crsp.datenum == thisDate & crsp.stockWeights>0 & crsp.Sell ==1));
                 
                 %number (integer) of new stocks purchased
-                newStocksPurchased = sum(crsp.buy(crsp.datenum == thisDate & crsp.stockWeight==0));
+                newStocksPurchased = nansum(crsp.Buy(crsp.datenum == thisDate & crsp.stockWeights==0));
                 
-                %weight invested in new purchases at this date
+                %weight invested in new purchases on this date
                 currentWeighting = (1 - sales)/ newStocksPurchased;
                 
                 %value assignment
-                crsp.stockWeight(i) = crsp.buy(i)*currentWeighting;
+                crsp.stockWeights(i) = crsp.Buy(i)*currentWeighting;
             
             else
-                crsp.stockWeight(i) = 0;
+                crsp.stockWeights(i) = 0;
             end
         end
         
